7bec7a4e021c229f1fcfb354c42b32d02f4a8c36
itmanco <itmanco@gmail.com>
2025-08-22 10:45:20 +0900

    feat: add gh-pages deploy config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ClassRoom/'
+    : '/',
   transpileDependencies: true,
    devServer: {
     port: 3000

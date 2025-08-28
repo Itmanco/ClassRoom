d878c17cfe5c8b17c661995aaa6e4385af65b7f7
itmanco <itmanco@gmail.com>
2025-08-28 13:53:38 +0900

    svg typo fixed

--- src/components/ClassroomManager.vue
+++ src/components/ClassroomManager.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="classroom-manager">
-
     <div class="login-button-container">
       <svg @click="toggleLoginStatus" 
            xmlns="http://www.w3.org/2000/svg" 
@@ -9,7 +8,7 @@
            class="bi bi-box-arrow-in-right" 
            viewBox="0 0 16 16"
            :class="{'logged-in': isLoggedIn}">
-        <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
+        <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
         <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
       </svg>
     </div>
@@ -45,7 +44,7 @@
     </div>
     
     <LoginModal :isVisible="showLoginModal" @close="showLoginModal = false" @login-success="handleLoginSuccess" />
-    
+
   </div>
 </template>
 
@@ -70,7 +69,7 @@ export default {
   name: 'ClassroomManager',
   components: {
     MyClassroom,
-    LoginModal
+    LoginModal,
   },
   data() {
     return { 
@@ -78,6 +77,7 @@ export default {
       isAnonymous: false, 
       showLoginModal: false,
       classroomName: "Java プログラミング①",
+      numberOfDesks: 9,
       masterStudentList: [], 
       allStudentsMap: new Map(), 
       tabs: [],
@@ -149,7 +149,8 @@ export default {
       },
       studentsUnsubscribe: null,
       tabsUnsubscribe: null,
-      unsubscribeAuth: null
+      unsubscribeAuth: null,
+      showStudentEditor: false,
     };
   },
   async created() {
